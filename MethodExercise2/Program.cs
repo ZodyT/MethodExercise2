4e62baf6caee10910e0eab175646c60c80cab215
ZodyT <Zoraida1988@yahoo.com>
2020-11-24 09:51:03 -0600

    Finished

---
+++ MethodExercise2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MethodExercise2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+            public static int Add(int num1, int num2)
+            {
+                return num1 + num2;
+            }
+            public static int Multiply(int num1, int num2)
+            {
+                return num1 * num2;
+            }
+
+            public static int Subtract(int num1, int num2)
+            {
+                return num1 - num2;
+            }
+            public static int Divide(int num1, int num2)
+            {
+                return num1 / num2;
+            }
+        
+    }
+}
